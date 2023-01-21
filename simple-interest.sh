37712f31f6027761b1e1891bc4a6396a98304532
sodynet <81561199+sodynet@users.noreply.github.com>
2023-01-21 21:12:00 +0800

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -4,9 +4,9 @@
 
    # Do not use this in production. Sample purpose only.
 
-   # Author: Upkar Lidder (IBM)
+   # Author: SETH (IBM)
    # Additional Authors:
-   # <your GitHub username>
+   # <sodynet>
 
    # Input:
    # p, principal amount
